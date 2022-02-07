d3262bd3e260ef08a3fce63bbeaa62a5f5cce100
mlaizure <maddi.laizure@gmail.com>
2022-02-07 20:10:49 +0000

    Update init.el with typescript

--- init.el
+++ init.el
@@ -29,15 +29,22 @@
 (global-whitespace-mode t)
 
 (setq column-number-mode t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (flycheck puppet-mode solarized-theme))))
+ '(package-selected-packages
+   (quote
+    (typescript-mode flycheck puppet-mode solarized-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(setq create-lockfiles nil)
+;; (setq auto-save-default nil)
+;; (setq make-backup-files nil)
